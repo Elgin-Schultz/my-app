bd48ebb8482d3bc050bf07ecd814f406744f3427
Elgin Schultz <elgin@elginschultz.com>
2022-11-20 20:49:52 +0100

    Setup

---
+++ src/components/Main/Main.js
@@ -0,0 +1,19 @@
+export default function Main() {
+  return (
+    <main className="main">
+      <div className="entries-form">
+        <input></input>
+        <text></text>
+        <button></button>
+      </div>
+      <div className="entries-section">
+        <div className="tab-bar"></div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </main>
+  );
+}
